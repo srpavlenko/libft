9f4756a20569ac8cd7bc30029a2f83ea50dcadc2
Artem Pavlenko <121798845+srpavlenko@users.noreply.github.com>
2023-01-30 23:08:50 +0100

    Add ft_strlcpy.c

---
+++ ft_strlcpy.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	srclen;
+	size_t	i;
+
+	srclen = ft_strlen(src);
+	i = 0;
+	if (dstsize == 0)
+		return (srclen);
+	while (*(src + i) && (i < (dstsize - 1)))
+	{
+		*(dst + i) = *(src + i);
+		i++;
+	}
+	*(dst + i) = '\0';
+	return (srclen);
+}
