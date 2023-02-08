e7576d280cc5ce7d10f8c38592f8c8c3bb381fe9
Artem Pavlenko <121798845+srpavlenko@users.noreply.github.com>
2023-02-08 18:51:09 +0100

    Add ft_memchr.c

---
+++ ft_memchr.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*str;
+	unsigned char	chr;
+	size_t			i;
+
+	str = (unsigned char *)s;
+	chr = (unsigned char)c;
+	i = 0;
+	while (i < n)
+	{
+		if (*(str + i) == chr)
+			return ((void *)(s + i));
+		i++;
+	}
+	return (NULL);
+}
