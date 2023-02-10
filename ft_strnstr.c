17095a016c6f503826ca6ecf6406589e692e835d
Artem Pavlenko <121798845+srpavlenko@users.noreply.github.com>
2023-02-10 18:52:49 +0100

    Add ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!(*needle))
+		return ((char *)haystack);
+	i = 0;
+	while (*(haystack + i) && i < len)
+	{
+		j = 0;
+		while (*(needle + j) && ((*(haystack + i + j) == *(needle + j)))
+			&& (i + j) < len)
+			j++;
+		if (!(*(needle + j)))
+			return ((char *)(haystack + i));
+		i++;
+	}
+	return (NULL);
+}
