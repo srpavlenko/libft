f064d893bfc841268b49805cae7da1b5197a7003
Artem Pavlenko <121798845+srpavlenko@users.noreply.github.com>
2023-02-14 23:32:44 +0100

    Add ft_calloc.c

---
+++ ft_calloc.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*p;
+
+	p = malloc(count * size);
+	if (p == NULL)
+		return (NULL);
+	ft_bzero(p, count * size);
+	return (p);
+}
