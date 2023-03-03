f06a99e51adc2cb1e5dd5c2bdf9b8e0bf70a2750
Artem Pavlenko <121798845+srpavlenko@users.noreply.github.com>
2023-03-03 23:50:20 +0100

    Add ft_lstnew_bonus.c

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*ptr;
+
+	ptr = malloc(sizeof(t_list));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->content = content;
+	ptr->next = NULL;
+	return (ptr);
+}
